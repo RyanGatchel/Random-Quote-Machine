5f7e0914bd3843a64aab0da212b5227e8418cf88
Ryan Gatchel <rgatchel94@gmail.com>
2017-06-08 22:13:59 -0400

    Create main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-$('.newQuote').on("click",function(){
-  $('.thequote').text();
-  $('.author').text();
+$('.newQuote').on("click",function(){ //start of function 
+  $('.thequote').text(); //button
+  $('.author').text(); //button
 $.ajax({
-  type: "GET",
-  url:  "https://random-quote-generator.herokuapp.com/api/quotes/random",
-  success: function(randomQuote){
-    $('.thequote').text(randomQuote.quote);
-    $('.author').text( "- " + randomQuote.author);
+  type: "GET", //calls API
+  url:  "https://random-quote-generator.herokuapp.com/api/quotes/random", //API
+  success: function(randomQuote){ // What happens when it works!
+    $('.thequote').text(randomQuote.quote); //You get random quote pulled from server
+    $('.author').text( "- " + randomQuote.author); //get author of quote! 
   },
   }); 
-});
+}); 
